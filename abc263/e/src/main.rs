eef78abbc089ad46049a25e8e396f349d9bc8769
minami.yoshihiko <nannany@hey.jp>
2022-08-23 23:29:31 +0900

    うまくいってそうではあるのだがmod計算がよくわからん

--- abc263/e/src/main.rs
+++ abc263/e/src/main.rs
@@ -4,8 +4,19 @@ fn main() {
         mut a : [i64; n-1]
     }
 
-    dp: vec<i64> = vec![0; n];
+    let mut dp: Vec<i64> = vec![0; n];
 
+    for i in (0..n - 1).rev() {
+        let ai = a[i];
 
+        let mut numerator: i64 = 0;
+        for j in 1..=ai {
+            numerator += dp[i + j as usize]
+        }
+        numerator += ai + 1;
+        dp[i] = numerator / ai;
+    }
+
+    println!("{}", dp[0]);
 }
 
