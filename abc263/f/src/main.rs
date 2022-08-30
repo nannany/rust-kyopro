5c96030b7608fee560dacbf91390d461d9af2705
minami.yoshihiko <nannany@hey.jp>
2022-08-30 23:51:12 +0900

    なんとなく骨子を組み立てる

--- abc263/f/src/main.rs
+++ abc263/f/src/main.rs
@@ -6,5 +6,25 @@ fn main() {
         c: [[u64; n]; 1 << n]
     }
 
+    let mut dp = vec![vec![0; 1 << n]; n + 1];
+    let mut max = vec![vec![0; 1 << n]; n + 1];
 
+    for i in 1..=n {
+        for j in 0..1 << n {
+            if (j / (1 << i - 1)) % 2 == 0 {
+                // 右側と対戦
+                dp[i][j] = max[i][j] + c[j][i - 1]; //todo
+            } else {
+                // 左側と対戦
+                dp[i][j] = max[i][j] + c[j][i - 1]; //todo
+            }
+
+            // maxの更新
+            if max[i][j] < max[i][j + 1] {
+                max[i][j] = max[i][j + 1]; //todo
+            }
+        }
+    }
+
+    println!("{}", dp[n].iter().max().unwrap()))
 }
