2640cdc06557f2c24427d21289adf37a83175d39
minami.yoshihiko <nannany@hey.jp>
2022-09-04 16:26:31 +0900

    ac

--- abc263/f/src/main.rs
+++ abc263/f/src/main.rs
@@ -1,5 +1,6 @@
 use proconio::*;
 
+// ac
 fn main() {
     input! {
         n: usize,
@@ -9,22 +10,25 @@ fn main() {
     let mut dp = vec![vec![0; 1 << n]; n + 1];
     let mut max = vec![vec![0; 1 << n]; n + 1];
 
-    for i in 1..=n {
-        for j in 0..1 << n {
-            if (j / (1 << i - 1)) % 2 == 0 {
-                // 右側と対戦
-                dp[i][j] = max[i - 1][j] + c[j][i - 1]; //todo
-            } else {
-                // 左側と対戦
-                dp[i][j] = max[i - 1][j] + c[j][i - 1]; //todo
+    for i in 1..=n { // i回戦目を表す
+        for j in 0..(1 << n) { // j番目のノードを表す
+            let unit = 1 << (i - 1);
+            if (j / unit) % 2 == 0 { // 右側と対戦
+                let counter = unit * ((j / unit) + 1);
+                dp[i][j] = max[i - 1][counter] + dp[i - 1][j];
+            } else { // 左側と対戦
+                let counter = unit * ((j / unit) - 1);
+                dp[i][j] = max[i - 1][counter] + dp[i - 1][j];
             }
 
             // maxの更新
-            if max[i][j] < max[i][j + 1] {
-                max[i][j] = max[i][j + 1]; //todo
+            let belong_unit = 1 << i;
+            let belong = belong_unit * (j / belong_unit);
+            if max[i][belong] < dp[i][j] + c[j][i - 1] {
+                max[i][belong] = dp[i][j] + c[j][i - 1];
             }
         }
     }
 
-    println!("{}", dp[n].iter().max().unwrap()))
+    println!("{}", max[n].iter().max().unwrap());
 }
