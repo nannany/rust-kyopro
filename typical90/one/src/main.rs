1d8208c8fc0bec7dc3860d56816ad066e0e5ea33
minami.yoshihiko <nannany@hey.jp>
2023-03-26 19:04:20 +0900

    とりあえず要素を取り出す

--- typical90/one/src/main.rs
+++ typical90/one/src/main.rs
@@ -1,4 +1,16 @@
+
+// https://atcoder.jp/contests/typical90/tasks/typical90_a
 fn main() {
+    proconio::input! {
+        n: usize,
+        l: u32,
+        k: usize,
+        a: [u32; n],
+    }
+
+    let mut elements: Vec<u32> = a.windows(2).map(|pair| pair[1] - pair[0]).collect();
+
+    println!("elements: {:?}", elements);
 
     println!("Hello, world!");
 }
