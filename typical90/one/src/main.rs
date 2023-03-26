dee57d002e9fc64c3458afc9eb400c6cf0a8b392
minami.yoshihiko <nannany@hey.jp>
2023-03-27 00:23:14 +0900

    2分法準備

--- typical90/one/src/main.rs
+++ typical90/one/src/main.rs
@@ -1,4 +1,3 @@
-
 // https://atcoder.jp/contests/typical90/tasks/typical90_a
 fn main() {
     proconio::input! {
@@ -10,7 +9,19 @@ fn main() {
 
     let mut elements: Vec<u32> = a.windows(2).map(|pair| pair[1] - pair[0]).collect();
 
+    let mut target = n/2;
+
+    let mut max = elements.iter().min();
+    while target < 1 {
+
+        for ele in elements {
+            // let mut bucket
+
+        }
+    }
+
     println!("elements: {:?}", elements);
+    println!("max: {:?}", max);
 
     println!("Hello, world!");
 }
