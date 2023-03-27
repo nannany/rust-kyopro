6d109cd9de55250b5cff77d93963dfbe9c06c5df
minami.yoshihiko <nannany@hey.jp>
2023-03-28 00:20:09 +0900

    正解が出たり出なかったり

--- typical90/one/src/main.rs
+++ typical90/one/src/main.rs
@@ -8,20 +8,41 @@ fn main() {
     }
 
     let mut elements: Vec<u32> = a.windows(2).map(|pair| pair[1] - pair[0]).collect();
+    elements.push(l - a[n - 1]);
+    elements.insert(0, a[0]);
 
-    let mut target = n/2;
 
-    let mut max = elements.iter().min();
-    while target < 1 {
+    let mut range = l / 2;
+    let mut target = range;
 
-        for ele in elements {
-            // let mut bucket
+    let mut max = *elements.iter().min().unwrap() as u32;
+    while range >= 1 {
+        println!("target: {:?}", target);
+        let mut count = 0;
+        let mut bucket = 0;
+        for ele in &elements {
+            bucket += ele;
+            println!("bucket: {:?}", bucket);
 
+            if bucket >= target as u32 {
+                bucket = 0;
+                count += 1;
+            }
+
+            if count == k + 1 {
+                break;
+            }
+        }
+
+        range /= 2;
+
+        if count == k + 1 {
+            max = target;
+            target += range;
+        } else {
+            target -= range;
         }
     }
 
-    println!("elements: {:?}", elements);
     println!("max: {:?}", max);
-
-    println!("Hello, world!");
 }
