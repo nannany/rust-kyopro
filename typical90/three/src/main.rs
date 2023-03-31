e31d4efad71e9011525c62e589226c253aa146c2
minami.yoshihiko <nannany@hey.jp>
2023-03-31 21:56:13 +0900

    ac

--- typical90/three/src/main.rs
+++ typical90/three/src/main.rs
@@ -1,17 +1,46 @@
 use std::collections::HashMap;
-use proconio::marker::*;
 
 fn main() {
     proconio::input! {
         n: usize,
-        e: [(Usize1, Usize1); n-1]
+        e: [(usize, usize); n-1]
     }
 
-    let mut adjacency_list:HashMap<usize, Vec<usize>> = HashMap::new();
+    let mut adjacency_list: HashMap<usize, Vec<usize>> = HashMap::new();
 
     for (a, b) in e {
         adjacency_list.entry(a).or_insert(Vec::new()).push(b);
+        adjacency_list.entry(b).or_insert(Vec::new()).push(a);
     }
 
-    println!("Hello, world!");
+    let (longest_point, _) = get_longest_point_and_distance(&adjacency_list, 1, 1);
+
+    let (_, longest_distance) = get_longest_point_and_distance(&adjacency_list, longest_point, longest_point);
+
+    println!("{}", longest_distance + 1);
+}
+
+fn get_longest_point_and_distance(
+    adjacency_list: &HashMap<usize, Vec<usize>>,
+    start_point: usize,
+    parent_point: usize,
+) -> (usize, usize) {
+    let mut longest_point = start_point;
+    let mut longest_distance = 0;
+
+    for &next_point in adjacency_list.get(&start_point).unwrap() {
+        if next_point == parent_point {
+            continue;
+        }
+
+        let (next_longest_point, next_longest_distance) =
+            get_longest_point_and_distance(adjacency_list, next_point, start_point);
+
+        if longest_distance < next_longest_distance + 1 {
+            longest_distance = next_longest_distance + 1;
+            longest_point = next_longest_point;
+        }
+    }
+
+    (longest_point, longest_distance)
 }
