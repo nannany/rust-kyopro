a6648a2f30fc3530e82ffd8ea1268cac067c0889
minami.yoshihiko <nannany@hey.jp>
2023-04-03 01:31:38 +0900

    模範解答まねてみたが、これでもだめっぽい。c++じゃないとダメか

--- typical90/six/src/main.rs
+++ typical90/six/src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     }
 
     // 各アルファベットについて、該当の順番から右のどの位置に出現するかを格納した配列
-    let mut position: Vec<Vec<i32>> = vec![vec![-1; n]; 26];
+    let mut position: Vec<Vec<usize>> = vec![vec![s.len(); n]; 26];
 
     for i in (0..n).rev() {
         let c = s.chars().nth(i).unwrap();
         let index = ALPHABETS.iter().position(|&x| x == c).unwrap();
-        position[index][i] = i as i32;
+        position[index][i] = i;
 
         if i != n - 1 {
             for j in 0..26 {
@@ -24,19 +24,17 @@ fn main() {
         }
     }
 
-    let mut count = 0;
+    let mut current_pos = 0;
 
     let mut answer = String::new();
 
-    while answer.len() != k {
-        for i in 0..26 {
-            // if position[i][count] + k as i32 - answer.len() as i32 == n as i32 {
-            //     answer.push_str(s.chars().skip(position[i][count] as usize - 1).collect::<String>().as_str());
-            //     return;
-            // }
-            if position[i][count] != -1 && position[i][count] as usize + k - answer.len() <= n {
-                answer.push(ALPHABETS[i]);
-                count = (position[i][count] + 1) as usize;
+    for i in 0..k {
+        for j in 0..26 {
+            let next_pos = position[j][current_pos] as i32;
+            let max_possible_length: i32 = n as i32 - next_pos - 1 + i as i32 + 1;
+            if max_possible_length >= k as i32 {
+                answer.push(ALPHABETS[j]);
+                current_pos = next_pos as usize + 1;
                 break;
             }
         }
