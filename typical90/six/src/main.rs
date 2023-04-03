79513daee3e2e01b8c0593ad7dae4c24e1b8a541
minami.yoshihiko <nannany@hey.jp>
2023-04-04 00:09:15 +0900

    ac

--- typical90/six/src/main.rs
+++ typical90/six/src/main.rs
@@ -1,5 +1,3 @@
-const ALPHABETS: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
 fn main() {
     proconio::input! {
         n: usize,
@@ -7,38 +5,19 @@ fn main() {
         s: String
     }
 
-    // 各アルファベットについて、該当の順番から右のどの位置に出現するかを格納した配列
-    let mut position: Vec<Vec<usize>> = vec![vec![s.len(); n]; 26];
+    let mut ans = String::new();
 
-    for i in (0..n).rev() {
-        let c = s.chars().nth(i).unwrap();
-        let index = ALPHABETS.iter().position(|&x| x == c).unwrap();
-        position[index][i] = i;
+    let mut deque = std::collections::VecDeque::new();
 
-        if i != n - 1 {
-            for j in 0..26 {
-                if j != index {
-                    position[j][i] = position[j][i + 1];
-                }
-            }
+    for (i, s) in s.char_indices() {
+        while deque.back().map_or(false, |&c| c > s) {
+            deque.pop_back();
+        }
+        deque.push_back(s);
+        if i + k >= n {
+            ans.push(deque.pop_front().unwrap());
         }
     }
 
-    let mut current_pos = 0;
-
-    let mut answer = String::new();
-
-    for i in 0..k {
-        for j in 0..26 {
-            let next_pos = position[j][current_pos] as i32;
-            let max_possible_length: i32 = n as i32 - next_pos - 1 + i as i32 + 1;
-            if max_possible_length >= k as i32 {
-                answer.push(ALPHABETS[j]);
-                current_pos = next_pos as usize + 1;
-                break;
-            }
-        }
-    }
-
-    println!("{}", answer);
+    println!("{}", ans);
 }
