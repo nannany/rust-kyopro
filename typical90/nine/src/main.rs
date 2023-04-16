e283d0e1c877acf67b321c5310afb7b3214cef75
minami.yoshihiko <nannany@hey.jp>
2023-04-16 22:31:36 +0900

    進めた

--- typical90/nine/src/main.rs
+++ typical90/nine/src/main.rs
@@ -13,22 +13,17 @@ fn main() {
                 continue;
             }
 
-            println!("({}, {}) -> ({}, {})", x_origin, y_origin, x, y);
+
         }
     }
 }
 
 //
 fn calculate_angle(x1: i64, y1: i64, x2: i64, y2: i64) -> f64 {
-    let x = x2 - x1;
-    let y = y2 - y1;
+    let x:f64 = (x2 - x1) as f64;
+    let y:f64 = (y2 - y1) as f64;
     let rad = y.atan2(x);
-    let deg = rad * 180.0 / std::f64::consts::PI;
-
-    if deg < 0.0 {
-        deg + 360.0
-    } else {
-        deg
-    }
+    rad * 180.0 / std::f64::consts::PI
 }
+
 
