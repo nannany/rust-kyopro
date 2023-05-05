3f8d25866c46fb7d685f6c5ecd35fda3beb9748c
minami.yoshihiko <nannany@hey.jp>
2023-05-05 22:42:03 +0900

    dpでやりかける

--- typical90/eleven/src/main.rs
+++ typical90/eleven/src/main.rs
@@ -9,32 +9,13 @@ fn main() {
     // dcsをdでソート
     dcs.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
-    let mut answer = 0;
-    // bit全探索
-    for i in 0..1 << n {
-        let money = hantei(i, &dcs, n);
+    // dpでやる
+    let mut dp = vec![[0; 5001]; n];
 
-        answer = answer.max(money);
+    if dcs[0].0 >= dcs[0].1 {
+        dp[0][dcs[0].1] = dcs[0].2;
     }
 
-    println!("{}", answer);
-}
-
-fn hantei(mask: i32, dcs: &Vec<(usize, usize, usize)>, n:usize) -> usize {
-    let mut money_counter = 0;
-    let mut time = 0;
-
-    for i in 0..n {
-        if mask >> i & 1 == 1 {
-            money_counter += dcs[i].2;
-            time += dcs[i].1;
-        }
-        if time > dcs[i].0 {
-            return 0;
-        }
-    }
-
-    return money_counter;
 }
 
 
