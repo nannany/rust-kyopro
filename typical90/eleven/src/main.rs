beb49af186b14cef2322dde68b7750cab8583986
minami.yoshihiko <nannany@hey.jp>
2023-05-05 22:30:41 +0900

    課題2まではいける

--- typical90/eleven/src/main.rs
+++ typical90/eleven/src/main.rs
@@ -1,35 +1,40 @@
 fn main() {
     proconio::input! {
         n: usize,
-        dcs: [(usize, usize, i64); n],
+        dcs: [(usize, usize, usize); n],
     }
 
     let mut dcs = dcs.clone();
+
     // dcsをdでソート
     dcs.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
     let mut answer = 0;
     // bit全探索
     for i in 0..1 << n {
-        let money = hantei(i, dcs);
+        let money = hantei(i, &dcs, n);
 
         answer = answer.max(money);
     }
+
+    println!("{}", answer);
 }
 
-fn hantei(mask: i32, dcs: ) -> i32 {
-    let mut moneyCounter = 0;
+fn hantei(mask: i32, dcs: &Vec<(usize, usize, usize)>, n:usize) -> usize {
+    let mut money_counter = 0;
     let mut time = 0;
 
     for i in 0..n {
-        if
         if mask >> i & 1 == 1 {
-            moneyCounter += dcs[i].1;
-            time += dcs[i].0;
+            money_counter += dcs[i].2;
+            time += dcs[i].1;
+        }
+        if time > dcs[i].0 {
+            return 0;
         }
     }
 
-    return moneyCounter;
+    return money_counter;
 }
 
 
